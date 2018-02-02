7df79168c6e8e2ada013750d63c188ef003eb8f2
poorniii <35651828+poorniii@users.noreply.github.com>
2018-02-02 16:12:46 +0530

    DIGITS.C
    count number of digits of an integer

---
+++ DIGITS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+lomg long n;
+int count=0;
+printf("enter an integer:");
+scanf("%d",&n);
+while(n!=0)
+{
+n\=10;
+++ count;
+}
+printf("number of digits:%d",count);
+return 0;
+}
